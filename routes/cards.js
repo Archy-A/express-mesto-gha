54a21dc8fb48739979d6c77905cace27724b7199
Artur Avtukhov <archy6754267@gmail.com>
2023-01-26 12:21:44 +0100

    Project14__tests_23

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const cardsController = require('../controllers/cards');
+const Constants = require('../utils/constants');
 
 router.get('/', cardsController.getCards);
 router.delete('/:id', cardsController.deleteCard);
-router.post('/', cardsController.createCard);
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().min(2).pattern(Constants.REGEXPHTTP),
+  })}), cardsController.createCard);
+
 router.put('/:id/likes', cardsController.likeCard);
 router.delete('/:id/likes', cardsController.dislikeCard);
 
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,12 @@ const usersController = require('../controllers/users');
 
 router.get('/me', usersController.getMe);
 router.get('/', usersController.getUsers);
+
 router.get('/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/),
-  })}),
-usersController.getUser);
+  })}), usersController.getUser);
+
 router.patch('/me', usersController.updateUser);
 router.patch('/me/avatar', usersController.updateAvaUser);
 router.patch('/:id', usersController.updateUser);
