567083563552969e129e8981c6c53f429a640bcc
Artur Avtukhov <archy6754267@gmail.com>
2023-01-26 09:04:01 +0100

    Project14__tests_17

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().min(2).pattern(Constants.REGEXPHTTP),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(30),
+    email: Joi.string().email(),
+    password: Joi.string().min(8).max(30),
   }),
 }),
 
