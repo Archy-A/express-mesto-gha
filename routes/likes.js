3d6000db27cf0943e11f47b1d21f88eb5a7d7195
Artur Avtukhov <archy6754267@gmail.com>
2023-01-26 13:07:24 +0100

    Project14__tests_29

--- routes/likes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const cardsController = require('../controllers/cards');
-
-router.put('/', cardsController.likeCard);
-router.delete('/', cardsController.dislikeCard);
-
-module.exports = router;
